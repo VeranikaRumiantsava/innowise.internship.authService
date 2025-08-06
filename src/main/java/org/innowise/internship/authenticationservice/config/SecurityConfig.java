872f05fd1801bab643644db1b592b9c4f2e0ec4f
rumronya <rum.ronya@gmail.com>
2025-08-06 12:17:46 +0300

    fixed chain

--- src/main/java/org/innowise/internship/authenticationservice/config/SecurityConfig.java
+++ src/main/java/org/innowise/internship/authenticationservice/config/SecurityConfig.java
@@ -21,7 +21,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    JwtAuthFilter jwtAuthFilter;
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
