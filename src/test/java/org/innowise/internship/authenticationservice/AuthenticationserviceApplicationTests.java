9fc86e95e6d983ef62abe5b8257f5fc0f5b89e1e
rumronya <rum.ronya@gmail.com>
2025-08-06 10:55:22 +0300

    base

---
+++ src/test/java/org/innowise/internship/authenticationservice/AuthenticationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.innowise.internship.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
